73d661deac6e46b6e0a12f8b696b4385b72a8723
Tiago César Oliveira <tiago.oliveira@messagebird.com>
2022-07-03 20:02:40 +0200

    Using a fluent interface to initialize the app

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,8 @@
-import express, {Express, NextFunction, Request, Response} from "express";
-import {debug} from "util";
+import AppInitializer from "./app.initializer";
 
-const app: Express = express();
 const port = 8000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Express + TypeScript server");
-});
-
-const server = app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
-
-process.on("SIGTERM", () => {
-    debug("SIGTERM signal received: closing HTTP server")
-    server.close(() => {
-        debug("HTTP server closed")
-    })
-})
-
-// Handling not found
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(404).send("Resource not found")
-})
-
-// Handling HTTP error
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack)
-    res.status(500).send("Internal server error")
-})
+new AppInitializer()
+    .registerRoutes()
+    .registerGenericRoutes() // For 404 and 500
+    .run(port)
