9b28d2f6cd752c17e4321eeebadc9e57ca6263b2
Tiago César Oliveira <tiago.oliveira@messagebird.com>
2022-07-03 20:27:37 +0200

    Files renaming

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,48 @@
-import AppInitializer from "./app.initializer";
-import dotenv from "dotenv"
+import express, {Express, NextFunction, Request, Response} from "express";
+import {debug} from "util";
 
-dotenv.config()
+class App {
+    private _app: Express
 
-let port = process.env.PORT as unknown as number;
+    constructor() {
+        this._app = express()
+    }
 
-if (port == undefined) {
-    console.error("Invalid value for environment variable PORT")
-    process.exit(1)
+    registerRoutes() {
+        this._app.get('/', (req: Request, res: Response) => {
+            res.send("Express + TypeScript server");
+        });
+
+        return this
+    }
+
+    registerGenericRoutes() {
+        // Handling not found
+        this._app.use((req: Request, res: Response, next: NextFunction) => {
+            res.status(404).send("Resource not found")
+        })
+
+        // Handling HTTP error
+        this._app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+            console.error(err.stack)
+            res.status(500).send("Internal server error")
+        })
+
+        return this
+    }
+
+    run(port: number) {
+        const server = this._app.listen(port, () => {
+            console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+        });
+
+        process.on("SIGTERM", () => {
+            debug("SIGTERM signal received: closing HTTP server")
+            server.close(() => {
+                debug("HTTP server closed")
+            })
+        })
+    }
 }
 
-new AppInitializer()
-    .registerRoutes()
-    .registerGenericRoutes() // For 404 and 500
-    .run(port)
+export default App
